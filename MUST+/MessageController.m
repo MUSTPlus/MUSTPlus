46b8e9a3fce498d0443378e36a33cb7cf14b0adb
Umi <baka999999999@vip.qq.com>
2017-08-27 18:56:39 +0800

    fix

--- MUST+/MessageController.m
+++ MUST+/MessageController.m
@@ -64,6 +64,13 @@
 }
 -(void)viewDidAppear:(BOOL)animated{
     [[self.tabBarController.tabBar.items objectAtIndex:3] setBadgeValue:nil];
+
+}
+-(void)viewWillAppear:(BOOL)animated{
+    self.tabBarController.tabBar.hidden =YES;
+}
+-(void)viewWillDisappear:(BOOL)animated{
+    self.tabBarController.tabBar.hidden =NO;
 }
 - (void)onSelectedTableRow:(RCConversationModelType)conversationModelType
          conversationModel:(RCConversationModel *)model
